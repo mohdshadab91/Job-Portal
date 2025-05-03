a7bf80ba9f589edd3e0db7d55bebb155fa7e3a58
mohdshadab91 <mohdshadab3155@gmail.com>
2025-05-03 11:43:28 +0530

    add to nodemon file into local repostory

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // imports
-const express = require("express");
+// const express = require("express");
+import express from "express";
 // rest object 
 const app = express();
 // routes
 app.get("/",(req,res) => {
-    res.send("<h1> Welcome to my 1st Job Portal</h1>");
+    res.send("<h1> Welcome to my Job Portal</h1>");
 });
 // lisent
 app.listen(8081, () =>{
